a07d1f72022dbe5dc3e8b5cbad86809bf8e2a3c8
mambs01 <matt@invincio.com>
2025-03-30 20:13:06 -0400

    Initial Commit

---
+++ RadioGardenLauncher/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RadioGardenLauncher
+//
+//  Created by Matthew Ambs on 3/30/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
